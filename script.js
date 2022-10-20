53991943d80481cb2c73e019756258474ce99aa5
surjo-me <116312411+surjo-me@users.noreply.github.com>
2022-10-21 02:25:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+// let j=1;
+// if(j){
+//     console.log(j);
+
+// } else if(j){
+//     console.log("see")
+// }
+let newArray = ['go', 'away'];
+newArray = ["tf","go", "away"];
+let g = '0';
+let l='6';
+
+
+
+
+console.log(newArray);
+console.log(`gooooooooo`);
